853119a38ca8e8e4101c7f804ffadc567b990101
Darsh Kejriwal <darsh.kejriwal@gmail.com>
2023-08-19 19:40:07 +0530

    initialized

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Nav } from './nav'
+
+export const Header = () => {
+    return (
+        <header>
+            <img src="logo.svg" alt="" />
+            <Nav />
+        </header>
+    )
+}
